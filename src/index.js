7ee2b628be06ec5919fbcf869433e3992a9879cf
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-05 17:15:25 +0330

    Add counter

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from "./components/counter";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
