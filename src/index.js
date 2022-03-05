d4cbabbaf861d275a22b2c45dba8face1f5eab46
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-05 18:29:57 +0330

    Add counters component

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'mdb-ui-kit/css/mdb.min.css';
-import Counter from "./components/counter";
+import Counters from "./components/counters";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <Counters />
   </React.StrictMode>,
   document.getElementById('root')
 );
