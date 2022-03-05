2de2cc4a7d2e4c60d8638a072fe8251f9c2540ba
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-05 21:28:14 +0330

    Add app render

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'mdb-ui-kit/css/mdb.min.css';
-import Counters from "./components/counters";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
