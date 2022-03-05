649922534d0e413a0f613bb904da300335341afd
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-05 18:05:03 +0330

    Done

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,29 +2,51 @@ import React, {Component} from "react";
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: [
+            'Amir',
+            'Ali',
+            'Nilo',
+            'Nili',
+        ],
     };
+
+    constructor() {
+        super();
+        // this.handleIncrement = this.handleIncrement.bind(this);
+        // this.handleDecrement = this.handleDecrement.bind(this);
+    }
 
     render() {
         return (
             <div>
                 <span className={this.badgeClasses()}>{this.state.count === 0 ? 'Zero' : this.state.count}</span>
-                <button className="m-2 btn btn-sm btn-secondary">+</button>
-                <button className="m-2 btn btn-sm btn-secondary" disabled={this.check()}>-</button>
-                <button className="m-2 btn btn-sm btn-danger">Delete</button>
+                <button onClick={() => this.handleIncrement} className="m-2 btn btn-sm btn-secondary">Increment</button>
+                <button onClick={() => this.handleDecrement} className="m-2 btn btn-sm btn-secondary" disabled={this.state.count === 0 ? true : null}>Decrement</button>
+                <button onClick={() => this.deleteProduct} className="m-2 btn btn-sm btn-danger">Delete</button>
             </div>
         );
     }
 
-    badgeClasses() {
-        let classes = "m-2 badge bg-";
-        classes += (this.state.count) === 0 ? 'warning' : 'primary';
-        return classes;
+    handleIncrement = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+    handleDecrement = () => {
+        this.setState({
+            count: this.state.count - 1
+        });
     }
 
-    check() {
-        const count = this.state.count;
-        return count === 0 ? true : null;
+    deleteProduct = () => {
+        console.log('Delete');
+    }
+
+    badgeClasses() {
+        let classes = "m-2 badge badge-";
+        classes += (this.state.count) === 0 ? 'warning' : 'primary';
+        return classes;
     }
 }
 
