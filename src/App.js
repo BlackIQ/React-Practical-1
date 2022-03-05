879fc3bf380272d1756b5543950601ed880777de
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-05 21:28:36 +0330

    Add navbar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import ReactDOM from "react-dom";
 import './App.css';
+import Counters from "./components/counters";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+          <Navbar />
+          <div className="container-fluid">
+              <Counters />
+          </div>
+      </React.Fragment>
   );
 }
 
