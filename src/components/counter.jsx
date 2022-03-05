5276bc8c25aafae588017dd8f9818dade8e12fa5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-05 21:04:08 +0330

    Move handlers in containers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,32 +6,20 @@ class Counter extends Component {
     };
 
     render() {
-        console.log(this.props);
         return (
             <div>
-                <h5>Counter #{this.props.id}</h5>
-                <span className={this.badgeClasses()}>{this.state.value === 0 ? 'Zero' : this.state.value}</span>
-                <button onClick={this.handleIncrement} className="m-2 btn btn-sm btn-secondary">Increment</button>
-                <button onClick={this.handleDecrement} className="m-2 btn btn-sm btn-secondary" disabled={this.state.value === 0 ? true : null}>Decrement</button>
-                <button onClick={() => this.props.onDelete(this.props.id)} className="m-2 btn btn-sm btn-danger">Delete</button>
+                <h5>Counter #{this.props.counter.id}</h5>
+                <span className={this.badgeClasses()}>{this.props.counter.value === 0 ? 'Zero' : this.props.counter.value}</span>
+                <button onClick={() => {this.props.onIncrement(this.props.counter)}} className="m-2 btn btn-sm btn-secondary">Increment</button>
+                <button onClick={() => {this.props.onDecrement(this.props.counter)}} className="m-2 btn btn-sm btn-secondary" disabled={this.props.counter.value === 0 ? true : null}>Decrement</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="m-2 btn btn-sm btn-danger">Delete</button>
             </div>
         );
     }
 
-    handleIncrement = () => {
-        this.setState({
-            value: this.state.value + 1
-        });
-    }
-    handleDecrement = () => {
-        this.setState({
-            value: this.state.value - 1
-        });
-    }
-
     badgeClasses() {
         let classes = "m-2 badge badge-";
-        classes += (this.state.value) === 0 ? 'warning' : 'primary';
+        classes += (this.props.counter.value) === 0 ? 'warning' : 'primary';
         return classes;
     }
 }
