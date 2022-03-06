0acfb71ee753ef79d3fa63726d9b480be7793828
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-06 18:58:38 +0330

    Move stuff in counters

--- src/App.js
+++ src/App.js
@@ -5,21 +5,10 @@ import Navbar from "./components/navbar";
 import counter from "./components/counter";
 
 class App extends Component {
-    state = {
-        counters: [
-            {id: 1, value: 4},
-            {id: 2, value: 1},
-            {id: 3, value: 0},
-            {id: 4, value: 0},
-            {id: 5, value: 0},
-            {id: 6, value: 0},
-        ]
-    };
-
     render() {
         return (
             <React.Fragment>
-                <Navbar all={this.state.counters.map(c => c.value)} />
+                <Navbar all="10" />
                 <div className="container-fluid">
                     <Counters
                         onReset={this.handleReset}
@@ -29,35 +18,6 @@ class App extends Component {
                 </div>
             </React.Fragment>
         )
-    }
-
-    handleIncrement = counter => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index].value++;
-        this.setState({counters});
-    }
-
-    handleDecrement = counter => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index].value--;
-        this.setState({counters});
-    }
-
-    handleDelete = (counterId) => {
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({counters: counters});
-    }
-
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c;
-        });
-        this.setState({
-            counters: counters
-        });
     }
 
     calculateAll() {
