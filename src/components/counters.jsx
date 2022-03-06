1188c4fe7811cebb989d45b2c885f045084b7dd2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-06 09:59:14 +0330

    Moving data

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,21 +3,12 @@ import Counter from "./counter";
 import counter from "./counter";
 
 class Counters extends Component {
-    state = {
-        counters: [
-            {id: 1, value: 4},
-            {id: 2, value: 0},
-            {id: 3, value: 0},
-            {id: 4, value: 0},
-            {id: 5, value: 0},
-            {id: 6, value: 0},
-        ]
-    };
-
     render() {
         return (
             <div>
-                <button onClick={this.handleReset} className="m-4 btn btn-primary">Reset</button>
+                <button
+                    onClick={this.props.onReset}
+                    className="m-4 btn btn-primary">Reset</button>
                 <ul>
                     {
                         this.state.counters.map(
@@ -38,35 +29,6 @@ class Counters extends Component {
                 </ul>
             </div>
         );
-    }
-
-    handleIncrement = counter => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index].value++;
-        this.setState({counters});
-    }
-
-    handleDecrement = counter => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index].value--;
-        this.setState({counters});
-    }
-
-    handleDelete = (counterId) => {
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({counters: counters});
-    }
-
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c;
-        });
-        this.setState({
-            counters: counters
-        });
     }
 }
 
